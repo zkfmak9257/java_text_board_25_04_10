26a5772dce1c45110b150c203c4d4de54239baff
zkfmak9257 <zkfmak9257@naver.com>
2025-04-12 16:11:49 +0900

    시작 메시지 출력

--- src/main/java/com/ksh/java/board/Main.java
+++ src/main/java/com/ksh/java/board/Main.java
@@ -4,6 +4,10 @@ package com.ksh.java.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("== 자바 텍스트 게시판 ==");
+    System.out.println("텍스트 게시판을 시작합니다.");
+    System.out.println("텍스트 게시판을 종료합니다.");
+    System.out.println("== 자바 텍스트 게시판 종료==");
+
   }
 }
